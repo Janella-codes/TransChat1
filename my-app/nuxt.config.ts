31133a8e67f011711da957ad9682ed7d3cd3f89e
janelle vos <janella5@gmail.com>
2023-10-04 09:12:48 -0700

    Router

--- my-app/nuxt.config.ts
+++ my-app/nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    $production: {
+      routeRules: {
+        '/**': { isr: true }
+      }
+    },
+    $development: {
+      //
+    }
+  })
 
-})
+function defineNuxtConfig(arg0: { $production: { routeRules: { '/**': { isr: boolean; }; }; }; $development: {}; }) {
+    throw new Error("Function not implemented.");
+}
--- src/components/IconHoverEffect.tsx
+++ src/components/IconHoverEffect.tsx
@@ -10,5 +10,5 @@ export function IconHoverEffect({ children, red = false}:
     IconHoverEffectProps) {
         const colorClasses = red ? "outline-red-400 hover:bg-red-200 group-hover-bg-red group-focus-visible:bg-red-200 focus-visible:bg-red-200" : "outline-gray-400 hover:bg-gray-200 group-hover-bg-gray group-focus-visible:bg-gray-200 focus-visible:bg-gray-200";
         
-        return <div className={` rounded-full p-2 transition-colors duration-200 ${colorClasses}`}>{children}</div>
+        return <div className={`rounded-full p-2 transition-colors duration-200 ${colorClasses}`}>{children}</div>
     }
